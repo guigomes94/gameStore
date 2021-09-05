d83f548f81e16e5dd1719d566b4fa6f9cc3d13d4
carlosnsilva <cmanoel17@gmail.com>
2021-09-05 16:34:52 -0300

    Subindo alteracoes'

--- src/test/java/testesAutomatizados/steps/ConsultaClientesSteps.java
+++ src/test/java/testesAutomatizados/steps/ConsultaClientesSteps.java
@@ -3,8 +3,16 @@ package testesAutomatizados.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 
 public class ConsultaClientesSteps {
+
+    private Response response;
+    private RequestSpecification request;
+    private String url;
+    private ValidatableResponse json;
 
     @Given("Dado o endpoint Get\\/clients")
     public void dado_o_endpoint_get_clients() {
