bcc71bddca6406dda325400842e55e0542beedd9
guigomes94 <guigomes94@outlook.com>
2021-09-27 12:12:01 -0300

    ajuste rota /clients

--- src/main/java/com/gamestore/security/JWTConfig.java
+++ src/main/java/com/gamestore/security/JWTConfig.java
@@ -32,7 +32,9 @@ public class JWTConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll().anyRequest()
+		http.csrf().disable().authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/clients").permitAll()
+		.antMatchers(HttpMethod.POST, "/login").permitAll().anyRequest()
 				.authenticated().and().addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTValidateFilter(authenticationManager())).sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
